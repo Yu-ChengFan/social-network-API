f2b1c52deba411fbb48f11de8db220ce3ef7940d
Corny Mac <kevin130366@gmail.com>
2022-01-16 15:51:36 -0700

    routes

--- server.js
+++ server.js
@@ -6,10 +6,17 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(require('./routes'));
 
-app.use(routes);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-// sync sequelize models to the database, then turn on the server
+mongoose.set('debug', true);
+
+
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
 });
